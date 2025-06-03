8b808bacfca181a3990184b72612b01603878680
Paul Mayne <pmayne@gmail.com>
2025-06-03 23:14:57 +0200

    Initial Commit

---
+++ DO-iPhone-Proto/DO_iPhone_ProtoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DO_iPhone_ProtoApp.swift
+//  DO-iPhone-Proto
+//
+//  Created by Paul Mayne on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct DO_iPhone_ProtoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
